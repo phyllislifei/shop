eb5a57684adf9e657ca3a115c49a3270ae7afaf4
lifei <2694176251@qq.com>
2021-07-22 20:27:14 +0800

    Login

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login_container">
+        11111111
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: #ccc;
+}
+</style>
