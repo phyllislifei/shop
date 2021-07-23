3c4ea17194970e243cdbff4635cec77b5dc6d881
lifei <2694176251@qq.com>
2021-07-23 19:00:53 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+/* 导入全局样式表 */
+import './assets/css/global.css'
+import './assets/css/font.css'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 这样每个vue组件都可以直接通过this发起http请求
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-
-]
-
 const router = new VueRouter({
-  routes:[
+  routes: [
+    { path: '/home', component: Home },
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login }
   ]
 })
 
+//  挂载路由导航收尾
+router.beforeEach((to, from, next) => {
+  /* to:将要访问的路径
+  from: 代表从哪个路径来
+  next: 是一个函数，表示放行 next():直接放行  next('/login') 强制跳转
+  */
+  if (to.path === '/login') {
+    next()
+  }
+  const tokenstr = window.sessionStorage.getItem('token')
+  if (!tokenstr) {
+    next('/login')
+  }
+  next()
+})
 export default router
